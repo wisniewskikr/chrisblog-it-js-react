308363904adfcbbf14655cb8eff2033370ba4b08
wisniewskikr <wisniewskikr@gmail.com>
2023-11-05 19:10:08 +0100

    Add Hello World page

--- architectures/gui/html/js-react-gui-html-helloworld-multiple/src/App.js
+++ architectures/gui/html/js-react-gui-html-helloworld-multiple/src/App.js
@@ -1,8 +1,14 @@
+import { useState } from "react"
 import { Init } from "./Init"
+import { HelloWorld } from "./HelloWorld";
 
 function App() {
+  const [start, setStart] = useState(true)
+
   return (
-    <Init/>
+    <>
+      { start ? <Init /> : <HelloWorld /> }
+    </>  
   );
 }
 
