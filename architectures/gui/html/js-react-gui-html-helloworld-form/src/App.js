bfc48a91666e8a11046f0519da3e25d326d0932e
wisniewskikr <wisniewskikr@gmail.com>
2023-11-06 08:07:10 +0100

    Init form

--- architectures/gui/html/js-react-gui-html-helloworld-form/src/App.js
+++ architectures/gui/html/js-react-gui-html-helloworld-form/src/App.js
@@ -1,8 +1,17 @@
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <div>
+        <img src="/images/helloworld.jpg" alt="Hello World"/>
+      </div>
+      <form>
+          <div>
+              <label for="name">Name:</label>
+              <input type="text" name="name"/>
+          </div>
+      </form>
+      <span id="message-text">Hello World</span>
+    </>
   );
 }
 
