2e70d5aef858e10b69ef69878586c8d33b1048c9
wisniewskikr <wisniewskikr@gmail.com>
2023-11-06 07:52:43 +0100

    Implement page NotFound

--- architectures/gui/html/js-react-gui-html-helloworld-multiple/src/App.js
+++ architectures/gui/html/js-react-gui-html-helloworld-multiple/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelloWorld } from "./HelloWorld";
+import { NotFound } from "./NotFound";
 import { Init } from "./Init"
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <BrowserRouter>
         <Routes>
             <Route path="/" element={ <Init /> } />
-            <Route path="/helloworld" element={ <HelloWorld /> } />           
+            <Route path="/helloworld" element={ <HelloWorld /> } />
+            <Route path="/*" element={ <NotFound /> } />            
         </Routes>
     </BrowserRouter>
   );
---
+++ architectures/gui/html/js-react-gui-html-helloworld-multiple/src/NotFound.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export function NotFound() {
+
+  return (
+    <>
+      <div>Page Not Found!</div>
+      <Link to="/">Back</Link>      
+    </>
+  )
+  
+}
