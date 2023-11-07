07b26173d860ca5638ed1abcd9e91918a0831631
wisniewskikr <wisniewskikr@gmail.com>
2023-11-07 09:00:04 +0100

    Init setMessages

--- architectures/gui/html/js-react-gui-html-helloworld-crud/src/pages/ListPage.js
+++ architectures/gui/html/js-react-gui-html-helloworld-crud/src/pages/ListPage.js
@@ -1,6 +1,18 @@
 import { NavigationComponent } from "../components/NavigationComponent";
+import { useEffect, useState } from "react"
 
 export function ListPage() {
+
+  const [messages, setMessages] = useState(() => {
+    const localValue = localStorage.getItem("MESSAGES")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("MESSAGES", JSON.stringify(messages))
+  }, [messages])
 
   return (
     <>
