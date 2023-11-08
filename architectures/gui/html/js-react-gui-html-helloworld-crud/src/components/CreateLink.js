3c4ca5e8e6e04413ccac6a0a3f8da4e150709a65
wisniewskikr <wisniewskikr@gmail.com>
2023-11-08 08:28:30 +0100

    Add redirection after Create

--- architectures/gui/html/js-react-gui-html-helloworld-crud/src/components/CreateLink.js
+++ architectures/gui/html/js-react-gui-html-helloworld-crud/src/components/CreateLink.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 export function CreateLink({ addMessage, newMessage }) {
 
     const onClick = () => {
-        addMessage(newMessage)
+        addMessage(newMessage)        
     }
 
     return (
-        <Link to="" onClick={onClick}>Create</Link>   
+        <Link to="/" onClick={onClick}>Create</Link>   
     )
   
   }
