594e7f3b91674e4a3d94435c8b682b1ebc049daa
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-11 11:47:57 +0200

    Implement if-else operator

--- technologies/conditional-rendering/js-react-gui-html-helloworld-if-else-statement/src/App.js
+++ technologies/conditional-rendering/js-react-gui-html-helloworld-if-else-statement/src/App.js
@@ -1,9 +1,11 @@
 function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+
+  if(2 > 1) {
+    return (<div>Hello World!</div>);
+  } else {
+    return (<div>False</div>);
+  }
+  
 }
 
 export default App;
