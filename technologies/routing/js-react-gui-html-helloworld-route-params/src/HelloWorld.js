216617057a54db150b6104a223c3d1d7cb07f5c0
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-12 08:59:27 +0200

    Implement params

--- technologies/routing/js-react-gui-html-helloworld-route-params/src/HelloWorld.js
+++ technologies/routing/js-react-gui-html-helloworld-route-params/src/HelloWorld.js
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
 
 const HelloWorld = () => {
+
     let { name } = useParams(); 
     return (<h2>HelloWorld {name}!</h2>);
+    
 };
 
 export default HelloWorld;
