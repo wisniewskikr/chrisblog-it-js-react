5ac86a21fbdf287d09cf17505db49a65a96e8792
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-13 08:50:59 +0200

    Implement useContext with consumer

--- technologies/hooks/js-react-gui-html-helloworld-hook-usecontext-with-consumer/src/ChildComponent.js
+++ technologies/hooks/js-react-gui-html-helloworld-hook-usecontext-with-consumer/src/ChildComponent.js
@@ -1,14 +1,13 @@
-import { useContext } from "react";
 import MyContext from "./MyContext";
 
 function ChildComponent() {
 
-  const message = useContext(MyContext);
-
   return (
-    <div>
-      {message}
-    </div>
+    
+    <MyContext.Consumer>
+      {(message) => <div>{message}</div>}
+    </MyContext.Consumer>
+
   );
 
 }
