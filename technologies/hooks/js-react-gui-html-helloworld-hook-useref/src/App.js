5db6f05d8672f1c71d35aae9f7b728c1b19d580e
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-13 20:11:37 +0200

    Implement useRef

--- technologies/hooks/js-react-gui-html-helloworld-hook-useref/src/App.js
+++ technologies/hooks/js-react-gui-html-helloworld-hook-useref/src/App.js
@@ -1,9 +1,22 @@
+import { useRef } from "react";
+
 function App() {
+
+  const helloWorldRef = useRef();
+
+  const fillRef = () => {
+    helloWorldRef.current.value = "Hello World!";
+  };
+
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <div>
+        <input ref={helloWorldRef}/>
+      </div>
+      <button onClick={fillRef}>Click</button>
+    </>
   );
+
 }
 
 export default App;
