8d0c67195b1fbb385b828d6abd4762977a85388b
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-13 12:23:22 +0200

    Implement onClick action

--- technologies/actions/js-react-gui-html-helloworld-actions-onclick-button/src/App.js
+++ technologies/actions/js-react-gui-html-helloworld-actions-onclick-button/src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <div>
+        Hello World number: {count}!
+      </div>
+      <button onClick={increment}>
+        Click
+      </button>
+    </>
   );
 }
 
