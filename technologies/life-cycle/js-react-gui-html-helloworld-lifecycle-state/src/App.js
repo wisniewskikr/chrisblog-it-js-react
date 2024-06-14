2a5a93183a7f3359dbe7bbcb377ea411f41ba5a6
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-14 12:47:03 +0200

    Implement state in lifecycle

--- technologies/life-cycle/js-react-gui-html-helloworld-lifecycle-state/src/App.js
+++ technologies/life-cycle/js-react-gui-html-helloworld-lifecycle-state/src/App.js
@@ -1,9 +1,29 @@
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+import { Component } from "react";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div>Hello World number: { this.state.count }!</div>
+        <button onClick={this.increment}>Increment</button>
+      </>
+    );
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }));
+  };
+  
 }
 
 export default App;
