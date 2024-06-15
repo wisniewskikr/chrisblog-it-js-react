d9c5ffa4d5d6199d23657d6bed8a39ab269740eb
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-15 18:00:37 +0200

    Implement form

--- technologies/forms/js-react-gui-html-helloworld-form/src/App.js
+++ technologies/forms/js-react-gui-html-helloworld-form/src/App.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+
 function App() {
+ 
+  const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setGreeting(`Hello World, ${name}!`);
+  };
+
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+    
+      <form onSubmit={handleSubmit}>        
+        <div>
+          <input
+            type="text"
+            value={name}
+            placeholder="Name..."
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+      {greeting && <p>{greeting}</p>}
+
+    </>
   );
+
 }
 
 export default App;
