3f8c3465271af5654c9824560932a2459345ebdf
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-15 13:43:22 +0200

    Init project

---
+++ technologies/life-cycle/js-react-gui-html-helloworld-lifecycle-componentwillunmount/src/App.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: null
+    };
+  }  
+
+  componentDidMount() {
+
+    const fetchData = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+      const result = await response.json();
+      return result.id;
+    };
+
+    fetchData().then((count) => {      
+      this.setState({
+        count: count
+      });
+    });
+
+  }
+
+  render() {   
+    return (
+      <div>
+        Hello World number: {this.state.count}!
+      </div>
+    );
+  }
+  
+}
+
+export default App;
