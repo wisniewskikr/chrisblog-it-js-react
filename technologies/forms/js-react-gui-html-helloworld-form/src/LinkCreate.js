7bd218d1039c911edf4f1ccf274b5185b47d498a
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-15 19:04:13 +0200

    Remove unnecessary files

--- technologies/forms/js-react-gui-html-helloworld-form/src/LinkCreate.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export function LinkCreate({ displayMessage }) {
-
-  return (
-    <Link to="" onClick={displayMessage}>Create</Link>
-  )
-
-}
--- technologies/forms/js-react-gui-html-helloworld-form/src/Message.js
+++
@@ -1,9 +0,0 @@
-import "./css/helloworld.css"
-
-export function Message({ newName }) {
-
-  return (
-    <div id="message-text">Hello World { newName }!</div>
-  )
-
-}
