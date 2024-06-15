98d2fdf426b7cff0b15096dede066cfb9783a7f5
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-15 13:40:25 +0200

    Implement componentDidUpdate method

--- technologies/life-cycle/js-react-gui-html-helloworld-lifecycle-componentdidupdate/src/App.js
+++ technologies/life-cycle/js-react-gui-html-helloworld-lifecycle-componentdidupdate/src/App.js
@@ -25,6 +25,22 @@ class App extends Component {
 
   }
 
+  componentDidUpdate(prevProps) {
+
+    const fetchData = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+      const result = await response.json();
+      return result.id;
+    };
+    
+    if (this.props.id !== prevProps.id) {
+      fetchData().then((count) => {
+        this.setState({ count: count });
+      });    
+    }    
+
+  }
+
   render() {   
     return (
       <div>
