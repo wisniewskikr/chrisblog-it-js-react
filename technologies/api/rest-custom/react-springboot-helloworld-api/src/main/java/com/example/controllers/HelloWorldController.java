b38a7c7e31939934ad416a1d8127d98e718cb7ac
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-19 12:39:40 +0200

    Implement CrossOrigin

--- technologies/api/rest-custom/react-springboot-helloworld-api/src/main/java/com/example/controllers/HelloWorldController.java
+++ technologies/api/rest-custom/react-springboot-helloworld-api/src/main/java/com/example/controllers/HelloWorldController.java
@@ -4,10 +4,12 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class HelloWorldController {
 
 	@GetMapping
