9bd3bc9a0adff6e3f306db3818d91adb14ca2dc4
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-22 08:38:51 +0200

    Implement CrossOrigin

--- technologies/security/react-springboot-oauth2/react-springboot-helloworld-api/src/main/java/com/example/controllers/HelloWorldController.java
+++ technologies/security/react-springboot-oauth2/react-springboot-helloworld-api/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,24 +1,39 @@
 package com.example.controllers;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("${react.url}")
 public class HelloWorldController {
 
 	@RequestMapping(value="/")
-	public String helloWorld() {		
-		return "Hello World!";		
+	public ResponseEntity<Map<String, String>> helloWorld() {
+
+		Map<String, String> response = Collections.singletonMap("message", "Hello World!");		
+		return ResponseEntity.ok(response);		
+		
 	}
 	
 	@RequestMapping(value="/user")
-	public String helloWorldUser() {		
-		return "Hello World User!";		
+	public ResponseEntity<Map<String, String>> helloWorldUser() {
+
+		Map<String, String> response = Collections.singletonMap("message", "Hello World, User!");		
+		return ResponseEntity.ok(response);		
+		
 	}
 	
 	@RequestMapping(value="/admin")
-	public String helloWorldAdmin() {		
-		return "Hello World Admin!";		
+	public ResponseEntity<Map<String, String>> helloWorldAdmin() {
+
+		Map<String, String> response = Collections.singletonMap("message", "Hello World, Admin!");		
+		return ResponseEntity.ok(response);		
+		
 	}
 	
 }
