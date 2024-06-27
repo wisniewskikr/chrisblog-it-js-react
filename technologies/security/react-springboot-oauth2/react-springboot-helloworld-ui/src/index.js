d9b41f25a0e0a113c2669c9aaa7c2f1e8c50a0dd
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-27 19:31:21 +0200

    Implement AuthProvider

--- technologies/security/react-springboot-oauth2/react-springboot-helloworld-ui/src/index.js
+++ technologies/security/react-springboot-oauth2/react-springboot-helloworld-ui/src/index.js
@@ -6,16 +6,27 @@ import HelloWorldNotSecured from './components/hello-world-not-secured/HelloWorl
 import HelloWorldSecuredUser from './components/hello-world-secured-user/HelloWorldSecuredUser';
 import HelloWorldSecuredAdmin from './components/hello-world-secured-admin/HelloWorldSecuredAdmin';
 import ErrorMessage from './components/error/ErrorMessage';
+import AuthProvider from 'react-auth-kit/AuthProvider';
+import createStore from 'react-auth-kit/createStore';
+
+const store = createStore({
+  authName:'_auth',
+  authType:'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'http:',
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={ <Home/>} />
-      <Route path="/not-secured" element={ <HelloWorldNotSecured/> } />
-      <Route path="/secured-user" element={ <HelloWorldSecuredUser/> } />
-      <Route path="/secured-admin" element={ <HelloWorldSecuredAdmin/> } />
-      <Route path="/error" element={ <ErrorMessage/>} />
-    </Routes>
-  </BrowserRouter>
+  <AuthProvider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home/>} />
+        <Route path="/not-secured" element={ <HelloWorldNotSecured/> } />
+        <Route path="/secured-user" element={ <HelloWorldSecuredUser/> } />
+        <Route path="/secured-admin" element={ <HelloWorldSecuredAdmin/> } />
+        <Route path="/error" element={ <ErrorMessage/>} />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
 );
