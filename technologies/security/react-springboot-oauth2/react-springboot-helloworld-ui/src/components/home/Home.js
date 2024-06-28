5226b43031a3902902d19edfe1d7c2fd8bb0ef1d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-28 12:51:32 +0200

    Init login/logout component

--- technologies/security/react-springboot-oauth2/react-springboot-helloworld-ui/src/components/home/Home.js
+++ technologies/security/react-springboot-oauth2/react-springboot-helloworld-ui/src/components/home/Home.js
@@ -1,15 +1,27 @@
+import useIsAuthenticated from "react-auth-kit/hooks/useIsAuthenticated";
+import useSignOut from "react-auth-kit/hooks/useSignOut";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   
+  const isAuthenticated = useIsAuthenticated();
+  const signOut = useSignOut();
+
   return (
-    <nav>
-      <ul>
-          <li> <Link to="/not-secured">Hello World Not Secured</Link> </li>
-          <li> <Link to="/secured-user">Hello World Secured for User</Link> </li>
-          <li> <Link to="/secured-admin">Hello World Secured for Admin</Link> </li>
-      </ul>
-    </nav>
+
+    <>
+      
+    { isAuthenticated ? <div><Link to="/" onClick={signOut}>Logout</Link></div> : <div><Link to="/login">Login</Link></div> }
+      <nav>
+        <ul>
+            <li> <Link to="/not-secured">Hello World Not Secured</Link> </li>
+            <li> <Link to="/secured-user">Hello World Secured for User</Link> </li>
+            <li> <Link to="/secured-admin">Hello World Secured for Admin</Link> </li>
+        </ul>
+      </nav>
+    </>
+
+
   );
 
 }
