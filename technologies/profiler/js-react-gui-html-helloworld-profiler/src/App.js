8654e4f01fcc7faaa67bbf2a080eb1cd5af1c5d9
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-29 07:46:05 +0200

    Implement React Profile

--- technologies/profiler/js-react-gui-html-helloworld-profiler/src/App.js
+++ technologies/profiler/js-react-gui-html-helloworld-profiler/src/App.js
@@ -1,7 +1,35 @@
+import React, { Profiler } from 'react';
+
+function onRenderCallback(
+  id, // the "id" prop of the Profiler tree that has just committed
+  phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+  actualDuration, // time spent rendering the committed update
+  baseDuration, // estimated time to render the entire subtree without memoization
+  startTime, // when React began rendering this update
+  commitTime, // when React committed this update
+  interactions // the Set of interactions belonging to this update
+) {
+  console.log({
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions
+  });
+}
+
+function HelloWorld() {
+  return <div>Hello, World!</div>;
+}
+
 function App() {
   return (
-    <div>
-      Hello World!
+    <div className="App">
+      <Profiler id="HelloWorld" onRender={onRenderCallback}>
+        <HelloWorld />
+      </Profiler>
     </div>
   );
 }
