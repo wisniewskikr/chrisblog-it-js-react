18a79ff862a25618b85e61f5bfa94c8f92e7d5b5
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-08 20:28:11 +0200

    Remove project

--- technologies/tests/js-react-gui-html-helloworld-tests-cypress/cypress/e2e/app.cy.js
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000');
-    cy.get("div").should('contain', 'Hello World!');
-  })
-})
--- technologies/tests/js-react-gui-html-helloworld-tests-cypress/src/App.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import App from './App'
-
-describe('<App />', () => {
-  it('renders', () => {
-    cy.mount(<App />);
-    cy.get("div").should('contain', 'Hello World!');
-  })
-})
