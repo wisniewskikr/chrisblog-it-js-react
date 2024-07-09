1ee773a93c8cb3948e3f67312a4d1da48336f529
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-09 12:04:01 +0200

    Update ChildComponent

--- technologies/from-child-to-parent/js-react-gui-html-helloworld-from-child-to-parent/src/ChildComponent.js
+++ technologies/from-child-to-parent/js-react-gui-html-helloworld-from-child-to-parent/src/ChildComponent.js
@@ -1,16 +1,19 @@
 const ChildComponent = ({ fromChild }) => {
-    // No local state in the child component
+    
     const dataToParent = () => {
-      // Directly pass the input value to the parent
-      fromChild(document.getElementById
-          ("inputField").value);
+      fromChild(document.getElementById("name").value);
+      document.getElementById("name").value = "";
     };
   
     return (
-      <div>
-        <input type="text" id="inputField" />
-        <button onClick={dataToParent}>Send</button>
-      </div>
+        <>
+            <div>
+                <input type="text" id="name" placeholder="Name..." />
+            </div>
+            <div>
+                <button onClick={dataToParent}>Send</button>
+            </div>
+        </>
     );
   };
   export default ChildComponent;
