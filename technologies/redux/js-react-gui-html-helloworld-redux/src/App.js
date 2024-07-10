37bd98a68f898d9a1d9d45fe60fc7a6f3a06b4a5
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-10 08:49:33 +0200

    Update App component

--- technologies/redux/js-react-gui-html-helloworld-redux/src/App.js
+++ technologies/redux/js-react-gui-html-helloworld-redux/src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import { connect } from "react-redux";
+import { increment, decrement } from "./1-actions";
+
+const App = ({ count, increment, decrement }) => {
   return (
     <div>
-      Hello World!
+      <h1>Hello World number: {count}!</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
-}
+};
 
-export default App;
+const mapDispatchToProps = {
+  increment,
+  decrement,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.counter.count, 
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
