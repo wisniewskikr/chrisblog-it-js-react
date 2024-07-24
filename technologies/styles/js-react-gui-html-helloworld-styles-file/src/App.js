a5b14db4b1fbbfdf239b3c571b064c617266b535
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-24 19:24:28 +0200

    Init project

---
+++ technologies/styles/js-react-gui-html-helloworld-styles-file/src/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div style={inlineStyles.message}>
+      Hello World!
+    </div>
+  );
+}
+
+const inlineStyles = {
+  message: {
+    color: "red",
+    fontSize: "24px",
+  },
+};
+
+export default App;
