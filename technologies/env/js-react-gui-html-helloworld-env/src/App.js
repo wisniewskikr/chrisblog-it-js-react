2adf052d3bde7bbb4cb0d9cd13abd2ad703b1ccc
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-10 08:42:06 +0200

    Add checking environment type

--- technologies/env/js-react-gui-html-helloworld-env/src/App.js
+++ technologies/env/js-react-gui-html-helloworld-env/src/App.js
@@ -1,5 +1,11 @@
 function App() {
 
+  if (process.env.NODE_ENV === 'development') {
+    console.log("You are running in development mode.")
+  } else if (process.env.NODE_ENV === 'production') {
+    console.log("You are running in production mode.")
+  }
+
   const message = process.env.REACT_APP_MESSAGE;
 
   return (
