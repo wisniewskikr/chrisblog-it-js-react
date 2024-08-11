55a1c89dc534e6a553810df56e9209c9257d2500
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-11 08:36:23 +0200

    Implement default redirect

--- technologies/routing/js-react-gui-html-helloworld-route-default-redirect/src/index.js
+++ technologies/routing/js-react-gui-html-helloworld-route-default-redirect/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import HelloWorld from './HelloWorld';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Navigate to="/helloworld" replace />} />
       <Route path="/helloworld" element={<HelloWorld/>} />
     </Routes>
   </BrowserRouter>
