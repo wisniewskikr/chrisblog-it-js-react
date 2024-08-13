2a5862ba77a22f869fd2a6b7b9604f74c29136ea
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-13 08:45:15 +0200

    Implement handleOnClick

--- technologies/routing/js-react-gui-html-helloworld-route-link-onclick/src/Home.js
+++ technologies/routing/js-react-gui-html-helloworld-route-link-onclick/src/Home.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Home = () => (
-    <Link to="/helloworld">Hello World</Link>
-);
+const Home = () => {
+    
+    const [url, setUrl] = useState("https://www.google.com");
+
+    const handleOnClick = () => {
+        setUrl("/helloworld");
+    }
+
+    return (
+        <Link to={url} onClick={handleOnClick}>Hello World</Link>
+    )
+
+};
 
 export default Home;
