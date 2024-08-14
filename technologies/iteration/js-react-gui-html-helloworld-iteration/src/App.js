5f420946a844602f476992002bc51e2b54d372d4
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-14 17:39:45 +0200

    Implement iteration

--- technologies/iteration/js-react-gui-html-helloworld-iteration/src/App.js
+++ technologies/iteration/js-react-gui-html-helloworld-iteration/src/App.js
@@ -1,8 +1,29 @@
 function App() {
+
+  const messages = [
+    {text: 'Hello World 1!'},
+    {text: 'Hello World 2!'},
+    {text: 'Hello World 3!'}
+  ];
+
+  const list = getList();
+
+  function getList() {
+
+    let list = [];
+
+    for (let message of messages) {
+      list.push(<li key={message.text}> {message.text} </li>)
+    }
+
+    return list;
+
+  }
+
   return (
-    <div>
-      Hello World!
-    </div>
+    <ul>
+      {list}
+    </ul>
   );
 }
 
