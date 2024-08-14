801c0b37ba92e26bb383cfc0e070ef901ba35763
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-14 12:24:33 +0200

    Init js in component

--- technologies/js/js-react-gui-html-helloworld-js-component/src/App.js
+++ technologies/js/js-react-gui-html-helloworld-js-component/src/App.js
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+
 function App() {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = './../assets/js/custom.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
       Hello World!
     </div>
   );
+
 }
 
 export default App;
